a599f75cd965178034ff737bf48d116e4c767fed
Shreysh Mane <shreysh.mane12@gmail.com>
2023-11-03 15:14:37 +0530

    ArrayMethods

--- Day3/index.js
+++ Day3/index.js
@@ -15,11 +15,11 @@
 
 // Arrays in js:
 
-let arr=[1,2,3,4,"Shreysh"];
+// let arr=[1,2,3,4,"Shreysh"];
 
 // console.log(arr);
 
-// console.log(arr[5].a);
+// console.log(arr[5]);
 
 // for of to access elements
 // for(let elements of arr){
@@ -27,27 +27,119 @@ let arr=[1,2,3,4,"Shreysh"];
 // }
 
 // for in to access index
-// for(let index in arr){
-//     console.log(index);
+// for(let i in arr){
+//     console.log(i);
 // }
 
 
 //forEach
-let sum=0;
-arr.forEach(function (element){
+// let sum=0;
+// arr.forEach(function (element,index, arr){
     //operation 
 
     // console.log(`Element: ${element}, Index: ${index}, ar: ${arr} `);
     // console.log(`Element: ${element} `);
-    sum+=element; 
-}
-)
+    // sum+=element; 
+// }
+// )
 // console.log(sum);
 
 
-arr.forEach((element,index)=>{
-    console.log(element);
-})
+// arr.forEach((element,index)=>{
+//     console.log(element);
+// })
+
+
+
+// Array Methods.
+let arrMeth=[1,2,3,4,5];
+
+// 1. pop();
+// let a=arrMeth.pop();
+// console.log(a);
+// console.log(arrMeth);
+
+// 2. push()
+
+// arrMeth.push("A");
+// console.log(arrMeth);
+
+
+// 3. shift()
+
+// arrMeth.shift();
+// console.log(arrMeth);
+
+// 4. unshift()
+
+// arrMeth.unshift(0);
+// console.log(arrMeth);
+
+// 5. indexOf(element);
+
+// console.log(arrMeth.indexOf(2));
+// console.log(arrMeth.indexOf(2));
+// console.log(arrMeth.lastIndexOf(2));
+
+// if(arrMeth.indexOf(2)==arrMeth.lastIndexOf(2)){
+//     console.log("present once");
+// }else{
+//     console.log("present multiple times");
+// }
+
+
+//Slice
+// first para is the start index 
+// second para is the end index + 1
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1,4);
+// console.log(citrus);
+// console.log(fruits);
+
+
+// Splice()
+
+// Splice for deleting element using index
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2,1);
+// console.log(fruits);
+
+// Splice for adding new element at specific index
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2,0,"a","b");
+// console.log(fruits);
+
+
+// const fruits = ["Banana","Orange", "Apple", "Mango"];
+// delete fruits[2];
+// console.log(fruits);
+
+// for(let ele of fruits){
+//     console.log(ele);
+// }
+
+//  toString()
+// let arr2=[1,2,3]
+// console.log(arr2.toString())
+
+
+// sort()
+
+// let arr3=[1,2,4,3,8,6,10,11,20];
+// console.log(
+//     arr3.sort(
+//         (a,b)=>a-b
+//         )
+//     );
+
+// let arr4=["jan","feb","march"]
+// console.log(arr4.sort());
+
+
+
+
+
+
 
 
 
