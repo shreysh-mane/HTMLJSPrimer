06d8bef362b5f9432c8648a6efa8705403dbdeea
Shreysh Mane <shreysh.mane12@gmail.com>
2023-11-06 15:58:41 +0530

    dom

---
+++ Day4/DOM.js
@@ -0,0 +1,15 @@
+
+
+// console.log(document);
+
+// console.log(document.body.childNodes);
+
+// console.log(document.body.children);
+
+// console.log(document.body.firstChild);  //text
+
+
+// console.log(document.body.firstElementChild); //div
+
+
+console.log(document.body.parentElement);
