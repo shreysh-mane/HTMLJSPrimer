3708777ef281678e59e3bba73e67e48a1890339e
Shreysh Mane <shreysh.mane12@gmail.com>
2023-11-07 12:33:29 +0530

    BubblingandCapturing

--- Day5/Dom2.js
+++ Day5/Dom2.js
@@ -1,13 +1,12 @@
 
 // Get Form data.
 
-let user={
-    uname:"",
-    upass:""
-}
-
-
 let form=document.getElementById('myForm');
+
+const ul=document.getElementById('userList');
+
+
+let users=[];
 
 function userRegistration(event){
 
@@ -15,14 +14,34 @@ function userRegistration(event){
     // Prevent form submission // prevent bydef form reloading.
 
    let name=document.getElementById('uname').value; 
+   document.getElementById('uname').value="";
 
    let pass=document.getElementById('upass').value;
 
-  
+    let user={
+        uname:"",
+        upass:""
+    }
+
    user.uname=name;
    user.upass=pass;
+   users.push(user);
+  
+//    ul.innerHTML +=`<li>${name}</li>`;
 
-    console.log(user);
+// Creating new HTML elements using JS
+
+// Create HTML element: document.createElement('<Element Name>');
+var newLi=document.createElement('li');
+
+// Add Contenet in Element:  newElement.innerHTML="";
+newLi.innerHTML=`${name}`;
+
+
+// Adding new element to parent element : parentEle.appendChild(<NewElemnt>);
+ul.appendChild(newLi);
+
+
 
 }
 
